d5e162bdd619e979786778de39dd34dd7d211ffa
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2018-04-23 15:58:27 +0900

    databaseのcolumnを変更

--- db/migrate/20180419103107_create_products.rb
+++ db/migrate/20180419103107_create_products.rb
@@ -1,8 +1,13 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps null: false
+      t.string :name
+      t.string :image
+      t.string :title
+      t.string :link
+      t.text :body
+      t.integer :price
+      t.timestamps
     end
   end
 end
